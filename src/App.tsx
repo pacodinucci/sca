d32322ca6392d7818b3d8d98320989b099f5b63a
pacodinucci <franciscoldinucci@gmail.com>
2025-07-02 15:53:08 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <img src="src/assets/sca_logo.png" alt="Logo SCA" />
+        <p className="coming-soon">Coming Soon</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
